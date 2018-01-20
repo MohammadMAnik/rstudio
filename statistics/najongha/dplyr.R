650a361afdba24bb31021598994f76470818f9ab
kimjh2807 <kimjh2807@gmail.com>
2018-01-20 23:01:33 +0900

    Update files

---
+++ statistics/najongha/dplyr.R
@@ -0,0 +1,3 @@
+# package = dplyr
+# data = hflights
+
--- statistics/najongha/regression.R
+++ statistics/najongha/regression.R
@@ -48,3 +48,59 @@ predict(model.step, list(Population=4000, Murder=10.5, HS.Grad=48, Frost=100))
 par(mfrow=c(2,2))
 plot(model.step)
 
+# data = Prestige
+# correlation, regression
+library(car)
+data(Prestige)
+str(Prestige)
+
+head(Prestige)
+summary(Prestige)
+
+# correlation visualization
+plot(Prestige[,1:4], pch=15, col="blue")
+
+# pairs.panel{psych}
+library(psych)
+pairs.panels(Prestige[,1:4], scale=TRUE)
+pairs.panels(Prestige[,1:4], bg=c("red", "yellow", "blue")[Prestige$type], pch=21)
+
+# model
+mod1 <- lm(income ~ education + prestige + women, data = Prestige)
+summary(mod1)
+
+mod2 <- lm(income ~ prestige + women, data = Prestige)
+summary(mod2)
+
+plot(mod2, pch = 16, which = 1)
+
+# data = mtcars
+data(mtcars)
+fit <- lm(mpg ~ disp + hp + wt + drat, data = mtcars)
+
+par(mfrow=c(2,2))
+plot(fit)
+par(mfrow=c(1,1))
+
+# outlier test
+library(car)
+outlierTest(fit)
+
+# qqplot
+qqPlot(fit, main = "QQ Plot")
+
+# leverage plot
+leveragePlots(fit)
+
+# influential point
+avPlots(fit)
+
+# cook's distance
+cutoff <- 4/((nrow(mtcars)-length(fit$coefficients)-2))
+plot(fit, which = 4, cook.levels = cutoff)
+
+# regression influence
+influencePlot(fit, id.method = "identify", main = "Influence Plot", 
+              sub = "Circle size is proportial to Cook's Distance")
+
+
