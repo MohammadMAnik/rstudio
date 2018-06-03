8ff9ac4c4dbddd45f798010fba12a293ffc03e81
kimjh2807 <kimjh2807@gmail.com>
2018-06-04 00:42:28 +0900

    modified file

--- ggplot2/Chapter2.R
+++ ggplot2/Chapter2.R
@@ -1,2 +1,11 @@
 library(ggplot2)
 
+mpg
+
+ggplot(mpg, aes(x = displ, y = hwy)) + geom_point()
+
+ggplot(mpg, aes(displ, hwy, colour = class)) + geom_point()
+
+ggplot(mpg, aes(displ, hwy, colour = class)) + geom_point(aes(colour = "blue"))
+ggplot(mpg, aes(displ, hwy, colour = class)) + geom_point(colour = "blue")
+
