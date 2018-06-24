cf1affd6eac2da28df720aa15828da0e7791e760
kimjh2807 <kimjh2807@gmail.com>
2018-06-24 16:18:39 +0900

    Modified file

--- siliconvalley/dplyr.R
+++ siliconvalley/dplyr.R
@@ -4,18 +4,18 @@ library(dplyr)
 
 # tbl_df
 iris
-i2 <- tbl_df(iris) # 앞에 5개 행만 출력
+i2 <- tbl_df(iris) # tbl_df()
 class(i2)
 i2
 
 # glimpse
-glimpse(i2)
+glimpse(i2) # all variable can see with transpose 
 
 # %>%
 iris %>% head
 iris %>% head(10)
 
-# filter
+# install "gapminder"
 install.packages("gapminder")
 library(gapminder)
 
@@ -23,6 +23,7 @@ gapminder <- tbl_df(gapminder)
 gapminder
 glimpse(gapminder)
 
+# filter()
 filter(gapminder, country=='Korea, Rep.')
 filter(gapminder, year==2007)
 filter(gapminder, country=='Korea, Rep.' & year== 2007)
@@ -31,20 +32,35 @@ gapminder %>% filter(country == 'Korea, Rep.')
 gapminder %>% filter(year == 2007)
 gapminder %>% filter(country == 'Korea, Rep.' & year == 2007)
 
-# arrange
+# arrange()
 arrange(gapminder, year, country)
 gapminder %>% arrange(year, country)
 
-# select
+# select()
 select(gapminder, pop, gdpPercap)
 gapminder %>% select(pop, gdpPercap)
 
-# mutate
+# mutate()
 gapminder %>% mutate(total_gdp = pop * gdpPercap,
                      le_gdp_ratio = lifeExp / gdpPercap,
                      lgrk = le_gdp_ratio * 100)
 
-# group_by
+# summarize()
+gapminder %>%
+  summarize(n_obs = n(),
+            n_countries = n_distinct(country),
+            n_year = n_distinct(year),
+            med_gdpc = median(gdpPercap),
+            max_gdppc = max(gdpPercap))
+
+# distinct()
+distinct(select(gapminder, country))
+distinct(select(gapminder, year))
+
+gapminder %>% select(country) %>% distinct()
+gapminder %>% select(year) %>% distinct()
+
+# group_by()
 gapminder %>%
   filter(year == 2007) %>%
   group_by(continent) %>%
@@ -55,3 +71,38 @@ gapminder %>%
   group_by(country) %>%
   summarise(lifeExp = median(lifeExp)) %>%
   arrange(-lifeExp)
+
+# join
+
+tbl_df(gapminder)
+
+distinct(select(gapminder, country))
+
+filter(gapminder, country == 'Korea, Rep.')
+filter(gapminder, year == 2007)
+filter(gapminder, country == 'Korea, Rep.' & year == 2007)
+
+gapminder %>% filter(country == 'Korea, Rep.') %>% filter(year == 2007)
+gapminder %>% filter(country == 'Korea, Rep.') %>% select(year, lifeExp)
+
+data_yl <- gapminder %>% filter(country == 'Korea, Rep.') %>% select(year, lifeExp)
+plot(data_yl)
+
+data_yp <- gapminder %>% filter(country == 'Korea, Rep.') %>% select(year, pop)
+plot(data_yp)
+
+data_ygpc <- gapminder %>% filter(country == 'Korea, Rep.') %>% select(year, gdpPercap)
+plot(data_ygpc)
+
+gapminder %>% mutate(total_gdp = pop * gdpPercap)
+
+# method 1
+d1 = filter(gapminder, year == 2007)
+d2 = group_by(d1, continent)
+d3 = summarize(d2, lifeExp = median(lifeExp))
+arrange(d3, -lifeExp)
+arrange(d1, -lifeExp)
+
+# method 2
+gapminder %>% filter(year == 2007) %>% group_by(continent) %>% 
+  summarize(lifeExp = median(lifeExp)) %>% arrange(-lifeExp)
