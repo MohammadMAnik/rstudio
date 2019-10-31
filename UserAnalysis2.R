02d3d9f263bdff42a10239dfd7d95df37dab673f
kimjh2807 <kimjh2807@gmail.com>
2019-10-31 21:22:03 +0900

    Update UserAnalysis2.R

--- UserAnalysis2.R
+++ UserAnalysis2.R
@@ -34,7 +34,7 @@ rel.gener <- (gender-min(gender))/(max(gender)-min(gender))
 grays <- gray(1-rel.gender)
 
 colors()
-grep("blue", colors(), value=TRUE)
+grep("red", colors(), value=TRUE)
 palette()
 
 # logical fill color
@@ -43,24 +43,23 @@ points(gender.over, col="red", pch=19)
 
 # age
 age <- c(18637, 52906, 37895, 24798, 10087, 2522)
-names(age) <- c("10대", "20대", "30대", "40대", "50대", "60대")
+names(age) <- c("10s", "20s", "30s", "40s", "50s", "60s")
 age
 
-plot(age)
+cols <- ifelse(age == max(age), "maroon2", "grey")
 
 # barplot
 barplot(age,
-        main="e-koreatech user on age",
-        xlab="Gender",
-        ylab="user count",
-        col=c("maroon", "cornflowerblue", "gray"),
+        main="e-koreatech 연령별 회원수",
+        xlab="연령대",
+        ylab="회원수",
+        ylim=c(0, 55000),
+        col=cols,
         legend=TRUE,
-        ylim=c(0, 120000),
         col.main="navy",
         col.lab="royalblue",
         col.axis="black",
         las=0)
-
 
 
 
