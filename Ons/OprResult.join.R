346b151c30aa7388369d1479980001aace995ed0
kimjh2807 <kimjh2807@gmail.com>
2019-11-13 15:41:08 +0900

    Update OprResult.join.R

--- Ons/OprResult.join.R
+++ Ons/OprResult.join.R
@@ -3,7 +3,7 @@
 getwd()
 setwd("D:/rstudio/ons")
 
-# inner join, two tables
+## inner join, two tables
 
 # CntList.csv
 CntList <- read.csv("CntList.csv", header=TRUE)
@@ -27,8 +27,12 @@ OprResult.join <- sqldf("select *
 
 write.csv(x=OprResult.join, file="OprResult.join.csv", row.names=FALSE)
 
-# load data
+## load data
 OprResult.join <- read.csv("OprResult.join.csv", header=TRUE)
+
+# load library
+library(dplyr)
+library(ggplot2)
 
 # summary()
 str(OprResult.join)
@@ -39,21 +43,38 @@ edu <- sum(OprResult.join$EduSum, na.rm = TRUE)
 round(comp/edu*100, 1)
 # 38.1%
 
-# 과정별 수료율
-Edu <- OprResult.join %>%
-  select(CntId, EduSum) %>%
+# 연도별 수료율
+OprResult.join %>%
+  select(Year, EduSum, CompleteSum) %>%
+  group_by(Year) %>%
+  summarise_each(funs(sum(., na.rm=TRUE))) %>%
+  mutate(Rate = round(CompleteSum/EduSum*100, 1)) %>%
+  arrange(Year)
+
+# 과정별 수료인원
+Content <- OprResult.join %>%
+  select(CntId, EduSum, CompleteSum) %>%
   group_by(CntId) %>%
-  summarise_each(funs(sum(., na.rm=TRUE)))
+  summarise_each(funs(sum(., na.rm=TRUE))) %>%
+  mutate(Rate = round(CompleteSum/EduSum*100, 1)) %>%
+  arrange(CntId)
 
-Comp <- OprResult.join %>%
-  select(CntId, CompleteSum) %>%
-  group_by(CntId) %>%
-  summarise_each(funs(sum(., na.rm=TRUE)))
+ggplot(Content, aes(CntId, Rate)) +
+  geom_point(aes(size=EduSum), alpha=1/3) +
+  geom_smooth()
 
-Success <- Edu %>% inner_join(Comp)
-Success
+# 과정별, 연도별, 운영실적
+Content_Year <- OprResult.join %>%
+  select(CntId, Year, EduSum, CompleteSum) %>%
+  group_by(CntId, Year) %>%
+  summarise_each(funs(sum(., na.rm=TRUE))) %>%
+  mutate(Rate = round(CompleteSum/EduSum*100, 1)) %>%
+  arrange(CntId)
 
-plot(Success$EduSum, Success$CompleteSum,
-     xlim=c(0, 1000),
-     ylim=c(0, 1000))
+Content_Year_Under10 <- Content_Year %>%
+  filter(CntId <= 10)
+
+ggplot(Content_Year_Under10, aes(Year, EduSum)) +
+  geom_line() +
+  facet_wrap(~ CntId)
 
