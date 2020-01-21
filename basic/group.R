c322e08ce7c6380df88f0d39c3a9e120dc44ee78
kimjh2807 <kimjh2807@gmail.com>
2020-01-21 17:46:06 +0900

    Create group.R

---
+++ basic/group.R
@@ -0,0 +1,31 @@
+# group
+
+mtcars
+car <- mtcars
+
+# label
+car <- within(car, am <- factor(am, levels = c(0, 1), labels = c("Automatic", "Manual")))
+
+# split()
+gear <- split(x=car$mpg, f=car$am)
+gear
+
+sapply(gear, length)
+sapply(gear, mean)
+
+# unstack()
+unstack(data.frame(car$mpg, car$am))
+
+# iris
+iris
+head(iris)
+
+# rowsum
+rowsum(x=iris[-5], group = iris$Species)
+
+# table
+table(car$gear)
+
+table(car$am)
+
+table(car$am, car$gear)
